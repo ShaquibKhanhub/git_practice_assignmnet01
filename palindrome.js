efd31ad20f0ae01cba2fd307e41b61a8ca560ce3
ShaquibKhanhub <ShaquibKhanrdx34@gmail.com>
2022-10-22 13:00:44 +0530

    code is update

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let str = "aman";
+let res = "";
+for(let i=0;i<=str.length-1;i++){
+    res+=str[i]
+}if( res=== str){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
---
+++ prime.js
@@ -0,0 +1,11 @@
+let num =13;
+let fact = 0;
+for(let i=1;i<=num;i++){
+    if(num%i){
+        fact++;
+    }
+}if(fact==2){
+    console.log("yes")
+}else {
+    console.log("No")
+}
